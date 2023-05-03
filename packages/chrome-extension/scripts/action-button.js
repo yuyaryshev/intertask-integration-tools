b19d975c281b892d7091081f6e9d758a929f2f7a
paveda <paveda@protonmail.com>
2023-05-03 14:34:34 +0000

    refactor style

--- packages/chrome-extension/scripts/action-button.js
+++ packages/chrome-extension/scripts/action-button.js
@@ -8,14 +8,28 @@ const actionButton = document.createElement('button');
 const wrapper = document.createElement('div');
 const statusText = document.createElement('p');
 
-actionButton.classList.add('action-button');
-wrapper.classList.add('action-button__wrapper');
-statusText.classList.add('action-button__status-text');
+actionButton.classList.add('iit__button', 'iit__button_on');
+wrapper.classList.add('iit__wrapper');
+statusText.classList.add('iit__status-text');
 
 actionButton.append(document.createTextNode('IIT'));
 wrapper.append(actionButton, statusText);
 document.body.append(wrapper);
 
-actionButton.addEventListener('click', () => {
-    window.postMessage({ type: "IIT_TOGGLE" }, "*")
-})
+actionButton.addEventListener('click', toggleIITForCurrentTab)
+
+async function toggleIITForCurrentTab() {
+    const currentTab = await getCurrentTab()
+
+    window.postMessage({
+        type: "IIT_TOGGLE",
+        tabId: currentTab.id,
+    }, "*")
+
+    actionButton.classList.toggle('iit__button_on');
+}
+
+async function getCurrentTab() {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    return tab;
+}
