edccf1350807e962f2195cdf8ce8b04071a6e843
Pavel Danilov <95907433+paveda@users.noreply.github.com>
2023-05-03 17:42:40 +0300

    Revert "try to connect with content script"
    This reverts commit 0c07ad21e5002b6be1aa27a76dc3c5d027d49922.

--- packages/chrome-extension/service-worker.js
+++ packages/chrome-extension/service-worker.js
@@ -4,15 +4,12 @@
 
 /// <reference types="@types/chrome" />
 
-const port = chrome.runtime.connect();
-
-window.addEventListener("message", (event) => {
-  if (event.source !== window) {
-    return;
-  }
-
-  if (event.data.type && (event.data.type === "IIT_TOGGLE")) {
-    console.log("Toggle IIT on tab with id: " + event.data.tabId);
-    port.postMessage(event.data);
-  }
-}, false);
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'sampleContextMenu',
+        title: 'Sample Context Menu',
+        contexts: [
+            'selection',
+        ]
+    })
+})
