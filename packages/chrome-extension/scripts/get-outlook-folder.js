a04d3967f863c36b8d66aae55ca594a24956a7ef
paveda <paveda@protonmail.com>
2023-05-09 14:59:19 +0000

    outlook plugin process

--- packages/chrome-extension/scripts/get-outlook-folder.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="@types/chrome" />
-
-fetch('https://owa.alfabank.ru/owa/service.svc?action=FindConversation&EP=1&ID=-35&AC=1').then((value) => {
-    console.log(value)
-
-    chrome.runtime.sendMessage({ type: "IIT_TOGGLE", value: 249328752938759 });
-})
--- packages/chrome-extension/scripts/outlook-plugin.js
+++
@@ -1,9 +0,0 @@
-    // const script = document.createElement('script');
-
-    // console.log("Hello!")
-
-    // script.textContent = `
-    //     console.log("Hello from injected script");
-    // `
-
-    // document.body.append(script)
